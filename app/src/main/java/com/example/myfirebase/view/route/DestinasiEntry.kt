7458f72947c6a12c9dc98fadcbc3d8558870c31c
Kevin Adrian <kevin19305.ib@gmail.com>
2026-01-02 14:21:01 +0700

    membuat file DestinasiEntry.kt untuk fungsi rute dari entry

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myfirebase.view.route
+
+import com.example.myfirebase.view.route.DestinasiNavigasi
+import com.example.myfirebase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
