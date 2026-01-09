a8d622dc939b3f19f63647daafb3e49e9c0ad48d
Kevin Adrian <kevin19305.ib@gmail.com>
2026-01-09 15:58:16 +0700

    menambahkan fungsi hapus untuk menghapus data saat ditekan hapus dan berfungsi tombolnya

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -23,23 +23,34 @@ class DetailViewModel(savedStateHandle: SavedStateHandle, private val repository
         savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
             ?: error("idSiswa tidak ditemukan di SavedStateHandle")
 
-    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
 
     init {
         getSatuSiswa()
     }
 
-    fun getSatuSiswa() {
+    fun getSatuSiswa(){
         viewModelScope.launch {
             statusUIDetail = StatusUIDetail.Loading
             statusUIDetail = try {
-                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa))
-            } catch (e: IOException) {
-                StatusUIDetail.Error
-            } catch (e: Exception) {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa) )
+            }
+            catch (e: IOException){
                 StatusUIDetail.Error
             }
+            catch (e: Exception){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+    suspend fun hapusSatuSiswa() {
+        try {
+            repositorySiswa.hapusSatuSiswa(idSiswa)
+            println("Sukses Hapus Data: $idSiswa")
+        } catch (e: Exception) {
+            println("Gagal Hapus Data: ${e.message}")
         }
     }
 }
